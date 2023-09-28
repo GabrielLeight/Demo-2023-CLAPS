e232f6294f6bdc37a1b45062a30c73e8118e8a6e
Juan Cucurella <juan.cucurella@sansano.usm.cl>
2023-09-28 00:37:07 -0300

    avance 1 registro

--- ClapsEnv/Scripts/activate.bat
+++ ClapsEnv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Moonlabpc\Desktop\infosoc proyecto\Claps\ClapsEnv
+set VIRTUAL_ENV=C:\Users\Moonlabpc\Desktop\infosoc\teatro\Teatro\ClapsEnv
 
 if not defined PROMPT set PROMPT=$P$G
 
