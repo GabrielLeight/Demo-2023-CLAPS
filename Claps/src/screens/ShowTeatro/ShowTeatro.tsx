8efa868cdd48bc95830d5c298cb54a89f488ed83
GabrielLeight <gabriel.leyton@usm.cl>
2023-10-12 20:04:28 -0300

    Showteatro se meolvdio

--- Claps/src/screens/ShowTeatro/ShowTeatro.tsx
+++ Claps/src/screens/ShowTeatro/ShowTeatro.tsx
@@ -4,40 +4,41 @@ import axios from 'axios';
 import { View, TextInput,StyleSheet, Text } from 'react-native';
 import CustomButton from '../../components/CustomButton';
 import CustomInput from '../../components/CustomInput';
+import ShowTeatro from '.';
 const TheaterCriticsForm: React.FC = () => {
-  const [criticName, setUsername] = useState('');
-  const [performanceTitle, setPerformanceTitle] = useState('');
-  const [rating, setRating] = useState(5); // Default rating
-  const [comments, setComments] = useState('');
+    const [criticName, getUsername] = useState('');
+    const [performanceTitle, setPerformanceTitle] = useState('');
+    const [rating, setRating] = useState(5); // Default rating
+    const [comments, setComments] = useState('');
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        
+        // Here, you can send the form data to your backend or perform any other action.
+        // For now, we'll just log the data to the console.
+        console.log('Critic Name:', criticName);
+        console.log('Performance Title:', performanceTitle);
+        console.log('Rating:', rating);
+        console.log('Comments:', comments);
     
-    // Here, you can send the form data to your backend or perform any other action.
-    // For now, we'll just log the data to the console.
-    console.log('Critic Name:', criticName);
-    console.log('Performance Title:', performanceTitle);
-    console.log('Rating:', rating);
-    console.log('Comments:', comments);
-
-
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setUsername(criticName);
+    
+        const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+            getUsername(criticName);
+          };
+        // You can also reset the form fields after submission.
+        getUsername('');
+        setPerformanceTitle('');
+        setRating(5);
+        setComments('');
       };
-    // You can also reset the form fields after submission.
-    setUsername('');
-    setPerformanceTitle('');
-    setRating(5);
-    setComments('');
-  };
-  const Enviar = async () => {
+const Enviar = async () => {
     try {
-      const response = await axios.post('YOUR_API_ENDPOINT', {
-        criticName,
-        performanceTitle,
-        rating: rating,
-        comments: comments,
-        is_active: true, // Set this as needed
+      const response = await axios.get('YOUR_API_ENDPOINT', {
+         //criticName:getUsername,
+         //performanceTitle,
+         //rating: rating,
+         //comments: comments,
+         //is_active: true, // Set this as needed
       });
 
       // Handle success, e.g., navigate to a new screen or display a success message
@@ -73,4 +74,4 @@ const TheaterCriticsForm: React.FC = () => {
 </View>
   );
 };
-export default TheaterCriticsForm;
+export default ShowTeatro
